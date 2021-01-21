aef351b59e4fc706234155edab94bf5421d37731
EUNSUN23 <60612115+EUNSUN23@users.noreply.github.com>
2021-01-21 20:26:32 +0900

    header컴포넌트화:url loader로 background url적용

--- frontend/src/components/Header/GNB/HeaderLogo.js
+++ frontend/src/components/Header/GNB/HeaderLogo.js
@@ -1,12 +1,10 @@
 import React from "react";
-import mainLogo from "../../../assets/images/revise_logo_main.png";
 
 const HeaderLogo = () => (
   <div className="logo">
     <div className="logo_inner">
       <h1>
         <a href="https://www.nl.go.kr/" alt="국립중앙도서관 메인페이지로 이동">
-          <img src={mainLogo} alt="main-logo" />
           <span className="hide">문화체육관광부 국앙중립도서관</span>
         </a>
       </h1>
