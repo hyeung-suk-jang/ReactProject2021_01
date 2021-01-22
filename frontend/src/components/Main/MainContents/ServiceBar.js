629fde9698a0f9b816261e7843848137b0f6054b
EUNSUN23 <60612115+EUNSUN23@users.noreply.github.com>
2021-01-22 16:15:36 +0900

    컴포넌트화 완료

--- frontend/src/components/Main/MainContents/ServiceBar.js
+++ frontend/src/components/Main/MainContents/ServiceBar.js
@@ -1,4 +1,12 @@
 import React from "react";
+import service1 from "../../../assets/images/service1.png";
+import service2 from "../../../assets/images/service2.png";
+import service3 from "../../../assets/images/service3.png";
+import service4 from "../../../assets/images/service4.png";
+import service5 from "../../../assets/images/service5.png";
+import service6 from "../../../assets/images/service6.png";
+import service7 from "../../../assets/images/service7.png";
+import service8 from "../../../assets/images/service8.png";
 
 const ServiceBar = () => (
   <div className="service">
@@ -6,49 +14,49 @@ const ServiceBar = () => (
       <ul>
         <li>
           <a href="">
-            <img src="./images/service1.png" alt="자료 당일 신청" />
+            <img src={service1} alt="자료 당일 신청" />
             <p>자료 당일신청</p>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="./images/service2.png" alt="도서관 방문 예약" />
+            <img src={service2} alt="도서관 방문 예약" />
             <p>도서관 방문 예약</p>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="./images/service3.png" alt="디지털도서관 예약" />
+            <img src={service3} alt="디지털도서관 예약" />
             <p>디지털도서관 예약</p>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="./images/service4.png" alt="온라인 견학" />
+            <img src={service4} alt="온라인 견학" />
             <p>온라인 견학</p>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="./images/service5.png" alt="이용자 교육" />
+            <img src={service5} alt="이용자 교육" />
             <p>이용자 교육</p>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="./images/service6.png" alt="편의시설" />
+            <img src={service6} alt="편의시설" />
             <p>편의시설</p>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="./images/service7.png" alt="찾아오시는 길" />
+            <img src={service7} alt="찾아오시는 길" />
             <p>찾아오시는 길</p>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="./images/service8.png" alt="전체보기/설정" />
+            <img src={service8} alt="전체보기/설정" />
             <p>전체보기/설정</p>
           </a>
         </li>
