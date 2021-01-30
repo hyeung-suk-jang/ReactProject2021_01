624b48c7be5bfc3a817633afa3c52d1215b8fc6e
EUNSUN23 <60612115+EUNSUN23@users.noreply.github.com>
2021-01-30 12:21:59 +0900

    로그인 에러수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import MyLibrary from "./components/MyLibrary";
 function App() {
   const history = useHistory();
   const [me, setMe] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
@@ -20,8 +21,10 @@ function App() {
         console.log("setMe");
         console.log(authService.currentUser.displayName);
         setMe(authService.currentUser.displayName);
+        setIsLoggedIn(true);
       } else {
         console.log("NO USER");
+        setIsLoggedIn(false);
       }
     });
   });
@@ -29,12 +32,29 @@ function App() {
   console.log(me);
   return (
     <Switch>
-      <Route exact path="/" render={() => <Home me={me} />} />
-      <Route exact path="/login" render={() => <Login me={me} />} />
-      <Route path="/detail:product" render={() => <Detail me={me} />} />
+      <Route
+        exact
+        path="/"
+        render={() => <Home isLoggedIn={isLoggedIn} me={me} />}
+      />
+      <Route
+        exact
+        path="/login"
+        render={() => <Login isLoggedIn={isLoggedIn} me={me} />}
+      />
+      <Route
+        path="/detail:product"
+        render={() => <Detail isLoggedIn={isLoggedIn} me={me} />}
+      />
       <Route
         path="/signup"
-        render={() => <SignUp history={history} me={me} />}
+        render={() => (
+          <SignUp history={history} isLoggedIn={isLoggedIn} me={me} />
+        )}
+      />
+      <Route
+        path="/mylib"
+        render={() => <MyLibrary isLoggedIn={isLoggedIn} me={me} />}
       />
     </Switch>
   );
